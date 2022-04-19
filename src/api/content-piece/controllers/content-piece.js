221bc97645fc633d816e89508ae0c515ea5258d1
Tomás <tomas.benitez.silva@gmail.com>
2022-04-19 19:19:50 -0300

    initial commit

---
+++ src/api/content-piece/controllers/content-piece.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  content-piece controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::content-piece.content-piece');
---
+++ src/api/content-piece/services/content-piece.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * content-piece service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::content-piece.content-piece');
